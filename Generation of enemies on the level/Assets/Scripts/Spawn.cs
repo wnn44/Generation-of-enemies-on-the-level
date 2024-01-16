3cff51d15f7beffaa446eb4601b7d47485f7debf
Nikolay Voronin <125467426+wnn44@users.noreply.github.com>
2024-01-16 16:55:04 +0300

    init spawn script

---
+++ Generation of enemies on the level/Assets/Scripts/Spawn.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+
+    [SerializeField] private List<GameObject> _spawnList = new List<GameObject>();
+    [SerializeField] private int _spawnTime;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
