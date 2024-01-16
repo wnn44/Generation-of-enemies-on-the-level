f44fb4f84cb773d0d12ca7040dd9f8fb874e9a77
Nikolay Voronin <125467426+wnn44@users.noreply.github.com>
2024-01-16 23:24:24 +0300

    Spawn, move

---
+++ Assets/Scripts/MoveEnemy.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MoveEnemy : MonoBehaviour
+{
+    [SerializeField] private GameObject _gameObject;
+    //public Vector3 pointToGo;
+    public float speed;
+
+    public bool colliding = false;
+
+    private void Update()
+    {
+        //if (!colliding)
+        {
+            //Move
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, _gameObject.gameObject.transform.position, step);
+            //Debug.Log(transform.position);
+        }
+    }
+
+    //Callback when enter the trigger
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        colliding = true;
+
+    }
+
+}
--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -1,19 +1,39 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
-
     [SerializeField] private List<GameObject> _spawnList = new List<GameObject>();
     [SerializeField] private int _spawnTime;
+    [SerializeField] private GameObject _spawnPrefab;
+    [SerializeField] private int _numberEnemies;
+
+    private Vector3 _positionSpawnPoint;
+    private Vector3 _positionCollectionPoint;
 
     private void Start()
     {
-        
+        StartCoroutine("SpawnEnemy");
     }
 
-    private void Update()
+    IEnumerator SpawnEnemy()
+    {        
+        while (_numberEnemies > 0)
+        {
+            SpawnNewEnemy();
+            yield return new WaitForSeconds(_spawnTime);
+            --_numberEnemies;
+        }
+    }
+
+    private void SpawnNewEnemy()
     {
+        int numberSpawner = Random.Range(0, _spawnList.Count);
         
+        _positionSpawnPoint = _spawnList[numberSpawner].gameObject.transform.position;
+        Instantiate(_spawnPrefab, _positionSpawnPoint, Quaternion.identity);
+
+        _positionCollectionPoint = _spawnList[numberSpawner].gameObject.transform.Find("CollectionPoint").position;
     }
 }
