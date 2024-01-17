52cf8b509f5e230145910c08c81220698569b53d
Nikolay Voronin <125467426+wnn44@users.noreply.github.com>
2024-01-17 21:51:09 +0300

    Move to points

--- Assets/Scripts/MoveEnemy.cs
+++ Assets/Scripts/MoveEnemy.cs
@@ -5,28 +5,45 @@ using UnityEngine.UIElements;
 
 public class MoveEnemy : MonoBehaviour
 {
-    [SerializeField] private GameObject _gameObject;
-    //public Vector3 pointToGo;
-    public float speed;
+    [SerializeField] private float _speed;
+    [SerializeField] private bool _colliding = false;
+    [SerializeField] private bool _goes = false;
 
-    public bool colliding = false;
+    private Vector3 _position;
+
+    private void OnEnable()
+    {
+        Spawn.SpawnPoint += Move;
+
+    }
+
+    private void OnDisable()
+    {
+        Spawn.SpawnPoint -= Move;
+    }
 
     private void Update()
     {
-        if (!colliding)
+        if (!_colliding)
         {
-            //Move
-            float step = speed * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(transform.position, _gameObject.gameObject.transform.position, step);
-            //Debug.Log(transform.position);
+            transform.position = Vector3.MoveTowards(transform.position, _position, _speed * Time.deltaTime);
         }
+    }
+
+    private void Move(Vector3 positionCollectionPoint)
+    {
+        if (!_goes)
+        {
+            _position = positionCollectionPoint;
+        }
+
+        _goes = true;
     }
 
     //Callback when enter the trigger
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        colliding = true;
-
+        _colliding = true;
     }
 
 }
--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,8 @@ public class Spawn : MonoBehaviour
     [SerializeField] private int _spawnTime;
     [SerializeField] private GameObject _spawnPrefab;
     [SerializeField] private int _numberEnemies;
+
+    public static event Action <Vector3> SpawnPoint;
 
     private Vector3 _positionSpawnPoint;
     private Vector3 _positionCollectionPoint;
@@ -22,18 +25,23 @@ public class Spawn : MonoBehaviour
         while (_numberEnemies > 0)
         {
             SpawnNewEnemy();
+
             yield return new WaitForSeconds(_spawnTime);
+
             --_numberEnemies;
         }
     }
 
     private void SpawnNewEnemy()
     {
-        int numberSpawner = Random.Range(0, _spawnList.Count);
+        int numberSpawner = UnityEngine.Random.Range(0, _spawnList.Count);
         
         _positionSpawnPoint = _spawnList[numberSpawner].gameObject.transform.position;
         Instantiate(_spawnPrefab, _positionSpawnPoint, Quaternion.identity);
-
+        
         _positionCollectionPoint = _spawnList[numberSpawner].gameObject.transform.Find("CollectionPoint").position;
+
+        SpawnPoint?.Invoke(_positionCollectionPoint);
+
     }
 }
